7d09c827e372b9c4c2aad677862c43e524d1f7d3
pverma93 <pverma93@in.ibm.com>
2021-06-20 17:28:29 +0530

    functional changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
-import {Routes , RouterModule} from '@angular/router';
-import { NgModule } from '../../node_modules/@angular/core';
-import { LoginComponent } from './login/login.component';
-import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "../../node_modules/@angular/core";
 
-const routes:Routes=[
-    {path:'login' , component:LoginComponent },
-    {path:'' ,redirectTo:"/login",pathMatch:'full' },
-    {path:'weather-detail', component: WeatherDetailComponent}
-  ];
+const routes: Routes = [];
 
-  @NgModule({
-      imports:[RouterModule.forRoot(routes)],
-      exports:[RouterModule]
-  })
-  export class AppRoutingModule{}
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
-import { WeatherDetailService } from './weather-detail.service';
-import {HttpClientModule} from '@angular/common/http';
-import {AppRoutingModule} from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { WeatherDetailComponent } from "./weather-detail/weather-detail.component";
+import { WeatherDetailService } from "./weather-detail.service";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    WeatherDetailComponent,
-   
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    AppRoutingModule
-
-  ],
+  declarations: [AppComponent, WeatherDetailComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [WeatherDetailService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
